29cd9275ee686630acb0e1b4b52ef8bd04ee8e9e
TothT <TothT@DESKTOP-B1149EG>
2025-01-14 22:30:31 +0100

    Add project files.

---
+++ BlackJack/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlackJack
+{
+    public partial class blackjack : Form
+    {
+        public blackjack()
+        {
+            InitializeComponent();
+        }
+
+        private void blackjack_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
